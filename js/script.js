c6e852b8c09659c66d546c568b1612bc64abf8cb
Latif Lumi <latiflumi12@gmail.com>
2025-09-13 17:34:49 +0200

    movie-details page updated with fetching from API

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ async function displayPopularMovies() {
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `
-         <a href="movie-details.html?${movie.id}">
+         <a href="movie-details.html?id=${movie.id}">
             ${
                 movie.poster_path 
                 ? `<img
@@ -43,7 +43,7 @@ async function displayPopularShows() {
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `
-         <a href="tv-details.html?${show.id}">
+         <a href="tv-details.html?id=${show.id}">
             ${
                 show.poster_path 
                 ? `<img
@@ -87,6 +87,69 @@ async function fetchAPIData(endpoint) {
     return data;
 }
 
+async function displayMovieDetails() {
+    const movieId = window.location.search.split('=')[1];
+    
+    const movie = await fetchAPIData(`movie/${movieId}`)
+
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="details-top">
+    ${
+       movie.poster_path
+    ? 
+    `
+    <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="Movie Title"
+            />
+    
+    `:
+    `<img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.name}"
+            />`
+    }  
+    <div>
+            
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}/ 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+                movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> </li>
+            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+            <li><span class="text-secondary">Status:</span> Released</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">Company 1, Company 2, Company 3</div>
+        </div>`;
+        
+        document.querySelector('#movie-details').appendChild(div);
+
+    
+}
+
 function showSpinner(){
     document.querySelector('.spinner').classList.add('show');
 }
@@ -105,6 +168,10 @@ function highlightActiveLink(){
     });
 }
 
+function addCommasToNumber(number){
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // Init App
 
 function init(){
@@ -117,6 +184,7 @@ function init(){
             displayPopularShows();
             break;
         case '/movie-details.html':
+            displayMovieDetails();
         break
         case '/tv-details.html':
             break;
